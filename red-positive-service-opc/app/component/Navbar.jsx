ed41ac691dc9b6f5bc49e920171b1a5be9dbc5be
LAKSHY RAJ SINGH DODIYA <111270574+Lakshy18@users.noreply.github.com>
2024-04-04 14:37:08 +0530

    Added my project

---
+++ red-positive-service-opc/app/component/Navbar.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+// import About from "./About";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between mb-2 pt-5 pb-5 bg-slate-800 text-slate-400">
+      <div>logo</div>
+      <div>About</div>
+    </div>
+  );
+};
+
+export default Navbar;
