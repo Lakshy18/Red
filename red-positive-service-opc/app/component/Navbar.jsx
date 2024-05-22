e576ae5bb79220302844b947ddc25c000104260e
LAKSHY RAJ SINGH DODIYA <111270574+Lakshy18@users.noreply.github.com>
2024-05-23 05:12:09 +0530

    Update Navbar.jsx

--- red-positive-service-opc/app/component/Navbar.jsx
+++ red-positive-service-opc/app/component/Navbar.jsx
@@ -1,10 +1,13 @@
-import Link from "next/link";
-// import About from "./About";
+"use client";
+import DeleteAllData from "../about/DeleteAllData";
+import RefreshData from "../about/RefreshData";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between mb-2 pt-5 pb-5 bg-slate-800 text-slate-400">
+     <div className="flex justify-between mb-2 pt-5 pb-5 bg-slate-800 text-slate-400"> 
       <div>logo</div>
+      <RefreshData/>
+      <DeleteAllData/>
       <div>About</div>
     </div>
   );
