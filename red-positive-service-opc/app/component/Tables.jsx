1ff68ccbd1ab4d2d26f7e460491e5bb8ca5b089b
LAKSHY RAJ SINGH DODIYA <111270574+Lakshy18@users.noreply.github.com>
2024-05-23 01:27:17 +0530

    Update Tables.jsx

--- red-positive-service-opc/app/component/Tables.jsx
+++ red-positive-service-opc/app/component/Tables.jsx
@@ -21,7 +21,7 @@ const Tables = () => {
     //   // });
     //   // console.log(tData);
     const asyncFunction = async () => {
-      const data = await axios.get("https://red-gx9ikti26-lakshy18s-projects.vercel.app/tableData");
+      const data = await axios.get("https://red-lakshy18s-projects.vercel.app/tableData");
       setTData(data.data);
       return;
     };
