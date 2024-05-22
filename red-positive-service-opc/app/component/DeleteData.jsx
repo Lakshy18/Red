8e29b465444f6bc7bbfe8eaf71a6d200cf2d667d
LAKSHY RAJ SINGH DODIYA <111270574+Lakshy18@users.noreply.github.com>
2024-05-23 00:58:02 +0530

    Update DeleteData.jsx

--- red-positive-service-opc/app/component/DeleteData.jsx
+++ red-positive-service-opc/app/component/DeleteData.jsx
@@ -9,7 +9,7 @@ const DeleteData = ({ id }) => {
   const handleClick = async () => {
     const confirmed = confirm("Are you Sure!");
     if (confirmed) {
-      await axios.delete(`http://localhost:4000/delTableData/${id}`);
+      await axios.delete(`https://red-gx9ikti26-lakshy18s-projects.vercel.app/delTableData/${id}`);
       router.refresh();
     }
   };
